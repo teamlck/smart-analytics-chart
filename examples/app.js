96c8aad85fdf29f4162df2dd62649a0c0aaf6f51
norux <andrew@norux.me>
2018-10-16 16:34:17 +0900

    Make simple tab for examples code

--- examples/app.js
+++ examples/app.js
@@ -1,1 +1,26 @@
 import './scss/index.scss'
+
+let nav = document.getElementById('navbar')
+nav.addEventListener('click', e => {
+  let target = e.target
+
+  switch (target.id) {
+    case 'home':
+      document.getElementById('contentHome').style.display = 'block'
+      document.getElementById('contentMouseClick').style.display = 'none'
+      document.getElementById('contentMouseMove').style.display = 'none'
+      break
+
+    case 'mouseClick':
+      document.getElementById('contentHome').style.display = 'none'
+      document.getElementById('contentMouseClick').style.display = 'block'
+      document.getElementById('contentMouseMove').style.display = 'none'
+      break
+
+    case 'mouseMove':
+      document.getElementById('contentHome').style.display = 'none'
+      document.getElementById('contentMouseClick').style.display = 'none'
+      document.getElementById('contentMouseMove').style.display = 'block'
+      break
+  }
+})
